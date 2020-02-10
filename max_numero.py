264a12b742bc65aea3e5baf9d0ada39456e66905
Andros <androsfuentes@gmail.com>
2020-02-10 00:57:52 -0600

    En este, ya hice mi primer programa con max()

---
+++ max_numero.py
@@ -0,0 +1,7 @@
+primer_num = int(input("dame un numero"))
+segundo_num = int(input("dame otro"))
+tercer_num = int(input("uno mas"))
+
+resultado = max(primer_num, segundo_num, tercer_num)
+
+print("el numero maximo es {}".format(resultado))
